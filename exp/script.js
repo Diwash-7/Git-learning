68bb3ea6507a0a917632f0090ed2b5fabd7c7e8e
Diwash-7 <rimaldiwash7@gmail.com>
2024-11-23 19:04:46 +0545

    version 1

---
+++ exp/script.js
@@ -0,0 +1,20 @@
+//Simple Multiplication Table Write a program that prints the multiplication 
+//table for a given number (from 1 to 10).
+
+function multiplicationTable(){
+    let n = prompt("Give a num")
+    for(let i=1;i<=10;i++){
+        console.log(n,"x",i,"=",i*n);
+    }
+}
+
+//Find the Smallest Number in an Array Write a function that takes an array
+// of numbers and returns the smallest number.
+
+// function smallestNum(){
+//     let n=[1,2,3,4,5]
+
+//     for(let i=0;i>=n.length;i++){
+//         if(n[i]<)
+//     }
+// }
---
+++ new/script.js
@@ -0,0 +1,23 @@
+let i=1
+let count=0
+
+function op() {
+   
+    let count = count+i
+console.log("hi")
+document.write()
+alert("hi")
+}
+function hi(){
+    prompt("hi")
+
+}
+
+let marks=[85,97,44,37,76,60]
+let sum=0;
+for(let val of marks){
+    sum=sum+val
+
+}
+let avg=sum/marks.length
+prompt(avg)
